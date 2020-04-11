e8f6668113631e11724d9c50af88712bd62bdf77
kotIIT <mkotlyar@hawk.iit.edu>
2020-04-11 02:35:30 -0500

    very good functionality

--- webInterface/app/static/index_handlers.js
+++ webInterface/app/static/index_handlers.js
@@ -1,9 +1,19 @@
 //check if js working
-console.log('pizza pasta put it in a box')
+console.log('pizza pasta put it in a box');
 
-$(function () {
+$(document).ready(function () {
 
     console.log('cheesie');
+
+    var deviceTable = new Tabulator("#device-table", {
+        height: "311px",
+        width:'600px',
+        columns: [
+            {title: "IP", field: "IP"},
+            {title: "MAC ADDRESS", field: "MAC ADDRESS"},
+            {title: "Manufacturer", field: "MANUFACTURER"},
+        ],
+    });
 
     $('a#shodanOpen').on('click', function () {
 
@@ -29,8 +39,20 @@ $(function () {
     $('a#devices').on('click', function () {
         $.get('/api/get_device_list',
             function (data) {
-                window.alert(data);
                 console.log(data);
+
+                for (var i in data.IP) {
+                    console.log(i);
+                    datum = {
+                        'IP': data.IP[i],
+                        'MANUFACTURER': data['MANUFACTURER'][i],
+                        'MAC ADDRESS': data['MAC ADDRESS'][i]
+                    };
+                    console.log(datum);
+
+                    deviceTable.addData([datum])
+                }
+
             });
         return false;
     });
