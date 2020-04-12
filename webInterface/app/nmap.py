ec068e4c89fa3c98f11d59c7841186e444f70f83
kotIIT <mkotlyar@hawk.iit.edu>
2020-04-12 02:23:52 -0500

    created beautiful Output Folder for everyone

--- webInterface/app/nmap.py
+++
@@ -1,6 +0,0 @@
-import nmap
-
-nm = nmap.PortScanner()
-#nm.scan(hosts='-iL Output/IPs.txt -F')
-nm.command_line('nmap -iL /Output/IPs.txt -F -oX Output/fastscan.xml')
-'nmap -iL /Output/IPs.txt -F -oX Output/fastscan.xml'
